b2da0b1b45a29d0cec7a59147b248df722b5e0f7
hwjeremy <hugh.jeremy@gmail.com>
2025-10-17 11:12:08 +1100

    fix bug preventing Human.Headline decode

--- procuret/human/headline.py
+++ procuret/human/headline.py
@@ -4,7 +4,9 @@ Human Headine Module
 author: hugh@blinkybeach.com
 """
 from procuret.data.codable import Codable, CodingDefinition as CD
+from typing import Any, TypeVar
 
+Self = TypeVar('Self', bound='HumanHeadline')
 
 class HumanHeadline(Codable):
 
@@ -26,3 +28,19 @@ class HumanHeadline(Codable):
 
     full_name = property(lambda s: s._full_name)
     agent_id = property(lambda s: s._agent_id)
+
+    @classmethod
+    def decode(self, data: Any) -> Self:
+
+        # Handle potentially divergent coding keys
+
+        if 'name' in data: 
+            return HumanHeadline(
+                agent_id=data['agent_id'],
+                full_name=data['name']
+            )
+        
+        return HumanHeadline(
+            agent_id=data['agent_id'],
+            full_name=data['full_name']
+        )
